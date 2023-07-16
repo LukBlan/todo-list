e719195889afe3b09c4577b9f880869e13fe4025
Lukblan <luk.blanko@gmail.com>
2023-07-15 21:51:55 -0300

    Generate input dynamically to create new project

--- src/newProjectButtonElement.js
+++ src/newProjectButtonElement.js
@@ -1,10 +1,25 @@
 export {newProjectButtonElement}
 
 const newProjectButtonElement = generateNewProjectButton() ;
-//const newProjectFormElement;
+const newProjectInputElement = generateInputElement();
 
 function generateNewProjectButton() {
-  const container = document.createElement("li");
-  container.innerText = "New Project";
-  return container;
+  const liElement = document.createElement("li");
+  const pElement = document.createElement("p");
+  pElement.innerText = "New Project";
+  pElement.classList.add("aside-element-text");
+  pElement.addEventListener("click", replaceButton)
+  liElement.append(pElement);
+  return liElement;
+}
+
+function replaceButton(event) {
+  event.target.parentElement.replaceChild(newProjectInputElement, event.target);
+}
+
+function generateInputElement() {
+  const newProjectInput =  document.createElement("input");
+  newProjectInput.classList.add("new-project-input");
+  newProjectInput.placeholder = "Project Name"
+  return newProjectInput;
 }
