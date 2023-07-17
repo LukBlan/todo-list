f266744c647b0472c5ffed698c2d6a57dfaa6951
Lukblan <luk.blanko@gmail.com>
2023-07-17 11:58:27 -0300

    Add and rename functions

--- src/asideViewController.js
+++ src/asideViewController.js
@@ -1,9 +1,20 @@
 import {newProjectButtonElement} from "./newProjectButtonElement.js"
-
-export {displayNewProjectButtonOnAside}
+export {displayNewProjectButton}
 
 const projectListDomElement = document.querySelector(".project-list");
 
-function displayNewProjectButtonOnAside() {
+function displayNewProjectButton() {
   projectListDomElement.append(newProjectButtonElement);
 }
+
+function removeNewProjectButton() {
+  projectListDomElement.removeChild(projectListDomElement);
+}
+
+/*
+function displayNewProject(newProjectDomElement) {
+  removeNewProjectButton();
+  newProjectDomElement.append(newProjectDomElement);
+  displayNewProjectButton();
+}
+ */
