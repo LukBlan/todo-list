5f76b379e846277c8164591a9aac102891c8a5ed
Lukblan <luk.blanko@gmail.com>
2023-08-23 11:09:16 -0300

    Add styles to Today and weekly projects

--- src/project/project-card-factory.js
+++ src/project/project-card-factory.js
@@ -4,7 +4,11 @@ class ProjectCardFactory {
   build(project) {
     const container = document.createElement("div");
     const projectNameElement = this.#createProjectTitle(project.name);
+    const todosElements = this.#createTodos(project.todoList);
+
+    container.classList.add("project-card")
     container.append(projectNameElement);
+    container.append(todosElements);
     return container;
   }
 
@@ -12,5 +16,21 @@ class ProjectCardFactory {
     const titleObject = document.createElement("h4");
     titleObject.innerText = project;
     return titleObject;
+  }
+
+  #createTodos(todoList) {
+    const container = document.createElement("ul");
+    container.classList.add("todo-list-element");
+    todoList.forEach(todo => {
+      const todoElement = this.#createTodo(todo.name)
+      container.append(todoElement);
+    })
+    return container;
+  }
+
+  #createTodo(todoName) {
+    const todoElement = document.createElement("li");
+    todoElement.innerText = todoName;
+    return todoElement;
   }
 }
